15fda7931c2c3b20899c6e7d5f0951b91b71fe61
Ewdokimova19 <57912351+Ewdokimova19@users.noreply.github.com>
2022-05-24 02:03:25 +0700

    Update main.js

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ function viewDiv(){
  if (addClick==1) document.getElementById("elem0").style.display = "block";   
   if (addClick==2) document.getElementById("elem1").style.display = "block";   
   if (addClick==3) document.getElementById("elem2").style.display = "block";   
-  if (addClick==4) document.getElementById("elem3").style.display = "block";
-   if (addClick==5) document.getElementById("elem3").style.display = "block";
-    if (addClick==6) document.getElementById("elem3").style.display = "block";
-   if (addClick==7) {document.getElementById("elem4").style.display = "block";  addClick = 0; }  
+  if (addClick==4) document.getElementById("elem4").style.display = "block";
+   if (addClick==5) document.getElementById("elem5").style.display = "block";
+    if (addClick==6) document.getElementById("elem6").style.display = "block";
+   if (addClick==7) {document.getElementById("elem7").style.display = "block";  addClick = 0; }  
 };
 
 function delNode(el){el.parentNode.parentNode.remove()}
