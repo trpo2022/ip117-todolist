88dfdc1a342a5f4668d1528d5610b1c096b56ab2
Ewdokimova19 <57912351+Ewdokimova19@users.noreply.github.com>
2022-05-30 00:11:49 +0700

    ubdate main.js

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 let addClick = 0;
 function viewDiv(){  
  updateDisplay(++addClick);
- console.log('add ' + addClick) ;
  if (addClick==1) document.getElementById("elem0").style.display = "block";   
   if (addClick==2) document.getElementById("elem1").style.display = "block";   
   if (addClick==3) document.getElementById("elem2").style.display = "block";   
@@ -14,5 +13,5 @@ function viewDiv(){
 function delNode(el){el.parentNode.parentNode.remove()}
 
 function updateDisplay(val) {
-    document.getElementById("counter-label").innerHTML = val;
+    console.log('add ' + addClick) ;
 }
